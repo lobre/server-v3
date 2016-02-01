d3eebad2e5210f80386f8d338fd4756e166f1b4f
Loric Brevet <loric.brevet@gmail.com>
2016-02-01 20:33:06 +0100

    Add proxy

---
+++ proxy/entrypoint.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+if [ -z "${SERVER}" ]; then
+  echo "set variable SERVER:"
+  echo "--env \"SERVER=server.example.com\""
+  exit 1
+fi
+
+if [ ! -z "${EMAIL}" ]; then
+  EMAIL="email@email.com"
+fi
+
+if [ ! -z "${ORGANIZATION}" ]; then
+  ORGANIZATION="org"
+fi
+
+if [ ! -z "${COUNTRY_CODE}" ]; then
+  COUNTRY_CODE="FR"
+fi
+
+if [ ! -z "${STATE}" ]; then
+  STATE="France"
+fi
+
+if [ ! -z "${LOCALITY}" ]; then
+  LOCALITY="Lyon"
+fi
+
+openssl req \
+  -new \
+  -nodes \
+  -x509 \
+  -subj "/C=${COUNTRY_CODE}/ST=${STATE}/L=${LOCALITY}/O=${ORGANIZATION}/CN=*.${SERVER}/emailAddress=${EMAIL}/subjectAltName=DNS.1=${SERVER}" \
+  -days 3650 \
+  -keyout /certificates/${SERVER}.key \
+  -out /certificates/${SERVER}.crt \
+  -extensions v3_ca \
